6591cfa06906a3a61d1a1cf3afb1709745626376
NEETU PAL <neetupal1702@gmail.com>
2026-08-07 10:11:21 +0000

    rest

--- LAST/srv/service.cds
+++ LAST/srv/service.cds
@@ -1,3 +1,16 @@
 service product{
     function printhelloworld (input:String) returns String;
+    function addition(num1: Integer, num2: Integer) returns Integer;
+
+    type CategoryInfo {
+        product : String;
+        price   : String;
+        location: String;
+        stock   : Integer;
+        priceArray : array of {
+            price : Integer;
+            discount : String;
+        };
+    }
+    function myfunction(category: Integer) returns CategoryInfo;
 }
--- LAST/srv/service.js
+++ LAST/srv/service.js
@@ -7,6 +7,32 @@ module.exports = cds.service.impl(async function (srv) {
        return `${req.data.input} world`
    })
 
-   
+   srv.on('addition', req => {
+       console.log(req.data)
+       let result = req.data.num1 + req.data.num2;
+       return result
+   })
+
+   srv.on('myfunction', req =>{
+    let result={}
+    if(req.data.category==1){
+        result.product ='BMW' ,
+        result.price ='1200 USD ',
+        result.location='new delhi'
+    }else {
+        result.product='audi',
+        result.stock= 120,
+        result.priceArray =[
+            {
+                "price": 475767,
+                "discount":"20%"
+            }
+        ]
+    }
+    console.log(result);
+    return result;
+   }
+
+   )
 })
 
